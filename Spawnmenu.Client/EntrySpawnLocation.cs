0ce80c182f508d68f1837519e40173f033525b0b
gaston11276 <johan.wilinger@gmail.com>
2021-12-15 16:09:42 +0100

    yml edit

--- Spawnmenu.Client/EntrySpawnLocation.cs
+++ Spawnmenu.Client/EntrySpawnLocation.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using Gaston11276.SimpleUi;
 using Gaston11276.Fivemui;
 
 namespace Gaston11276.Spawnmenu.Client
